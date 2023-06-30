53328b1da5105fb24c84ef3a69a7d5b080a5a309
pragati18 <pragati18.gaikwad@gmail.com>
2023-06-30 13:08:46 +0530

    User Authentication project with Django

---
+++ DjangoUserAuthentication/userauth/userauth_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class UserauthAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'userauth_app'
---
+++ DjangoUserAuthentication/userauth/userauth_app/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+# SET THE NAMESPACE!
+app_name = 'userauth_app'
+
+# Be careful setting the name to just /login use userlogin instead!
+urlpatterns=[
+    path('register/',views.register,name='register'),
+    path('user_login/',views.user_login,name='user_login'),
+]
+
+
+
+
